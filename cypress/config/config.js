13c100c80bc8d696128042c20b3d4cbc55220228
Mohammad Azwad Rashid <m.azwad.rashid@gmail.com>
2023-03-27 17:09:57 +0100

    Add config file to contain all hardcoded values
    This file contains all variables that might be required for any testing througout. This eliminates the need to have hardcoded values within specific tests
    
    Signed-off-by: Mohammad Azwad Rashid <m.azwad.rashid@gmail.com>

---
+++ cypress/config/config.js
@@ -0,0 +1,12 @@
+const config = {
+  loginUrl: 'https://www.hudl.com/login?forward=%2Flibrary%2F320073',
+  username: 'm.azwad.rashid@gmail.com',
+  password: 'D_Sr.aJ6w#z#Dg4',
+  dashboardUrl: '/new_library/t/320073',
+  userIconSelector: '.hui-globaluseritem',
+  errorMessageSelector:
+    '.styles_errorDisplayInnerContainer_3R2ni-zSvPIKWfKXiviJhH',
+  errorMessage: "We didn't recognize that email and/or password.Need help?",
+};
+
+module.exports = config;

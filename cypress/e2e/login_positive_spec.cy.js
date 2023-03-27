026b91ea527684a7634771f25d88cec9e8f154de
Mohammad Azwad Rashid <m.azwad.rashid@gmail.com>
2023-03-27 16:04:30 +0100

    Add Assertion statements to validate login
    Two assertion statements have been added to verify that the login was successful.
    
    The first one checks for a change in URL. Once successfully logged in the URL should end in /new_library/t/320073 for the user credential provided.
    
    The second assertion checks the presense of the user icon on the top right of the page that allows the user to navigate to the user's settings and profile.
    
    Signed-off-by: Mohammad Azwad Rashid <m.azwad.rashid@gmail.com>

--- cypress/e2e/login_positive_spec.cy.js
+++ cypress/e2e/login_positive_spec.cy.js
@@ -2,14 +2,20 @@ import LoginPage from '../pages/loginPage';
 
 describe('Login Positive Test', () => {
   it('Successfully logs in', () => {
+    // creates object for login page
     const loginPage = new LoginPage();
+
+    // instructs browser to visit login page
     loginPage.visit();
 
+    // Sets login credential
     const email = 'm.azwad.rashid@gmail.com';
     const password = 'D_Sr.aJ6w#z#Dg4';
 
     loginPage.login(email, password);
 
-    // Add assertions to check for successful login
+    // assertion to check successful login
+    cy.url().should('include', '/new_library/t/320073'); // Check change in URL
+    cy.get('.hui-globaluseritem').should('exist'); // Check that User Icon is present on new page
   });
 });

4c1f7a505fc0f10d63af51abd6b78eca0584dd85
Mohammad Azwad Rashid <m.azwad.rashid@gmail.com>
2023-03-27 16:10:16 +0100

    Move hardcoded text into variables
    Signed-off-by: Mohammad Azwad Rashid <m.azwad.rashid@gmail.com>

--- cypress/e2e/login_positive_spec.cy.js
+++ cypress/e2e/login_positive_spec.cy.js
@@ -11,11 +11,13 @@ describe('Login Positive Test', () => {
     // Sets login credential
     const email = 'm.azwad.rashid@gmail.com';
     const password = 'D_Sr.aJ6w#z#Dg4';
+    const newUrl = '/new_library/t/320073';
+    const userIconTag = '.hui-globaluseritem';
 
     loginPage.login(email, password);
 
     // assertion to check successful login
-    cy.url().should('include', '/new_library/t/320073'); // Check change in URL
-    cy.get('.hui-globaluseritem').should('exist'); // Check that User Icon is present on new page
+    cy.url().should('include', newUrl); // Check change in URL
+    cy.get(userIconTag).should('exist'); // Check that User Icon is present on new page
   });
 });
